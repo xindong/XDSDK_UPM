cc4a5e6f174026938d47f6fc2a6ae0a6a01b1c16
Sun Yi <sunyi@xindong.com>
2017-08-09 16:58:37 +0800

    split XDSDK.cs

---
+++ Unity/Assets/Plugins/script/XDSDKListener.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace xdsdk{
+	
+	public class XDSDKListener : MonoBehaviour {
+
+		void Start () {
+			this.name = "XDSDK";
+		}
+
+		//callback
+		public void OnInitSucceed (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnInitSucceed ();
+		}
+
+		public void OnInitFailed (string msg){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnInitFailed (msg);
+		}
+
+		public void OnLoginSucceed (string token){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnLoginSucceed (token);
+		}
+
+		public void OnLoginFailed (string msg){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnLoginFailed (msg);
+		}
+
+		public void OnLoginCanceled (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnLoginCanceled ();
+		}
+
+		public void OnGuestBindSucceed (string token){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnGuestBindSucceed (token);
+		}
+
+		public void OnLogoutSucceed (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnLogoutSucceed ();
+		}
+
+		public void OnPayCompleted (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnPayCompleted ();
+		}
+
+		public void OnPayFailed (string msg){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnPayFailed (msg);
+		}
+
+		public void OnPayCanceled (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnPayCanceled ();
+		}
+
+		public void OnExitConfirm (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnExitConfirm ();
+		}
+
+		public void OnExitCancel (){
+			XDSDKImp.GetInstance ().GetXDCallback ().OnExitCancel ();
+		}
+
+	}
+}
