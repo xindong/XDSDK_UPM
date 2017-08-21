a0916a6d0b2f895ce916097a1d0a2fa083b5c300
Sun Yi <sunyi@xindong.com>
2017-08-21 12:10:17 +0800

    update Android SDK`

--- Unity/Assets/Demo.cs
+++ Unity/Assets/Demo.cs
@@ -20,7 +20,7 @@ public class Demo : MonoBehaviour {
 		}
 
 		if (GUI.Button (new Rect (50, 300, 300, 100), "初始化", myButtonStyle)){
-			xdsdk.XDSDK.InitSDK ("a4d6xky5gt4c80s", 1);
+			xdsdk.XDSDK.InitSDK ("a4d6xky5gt4c80s", 0);
 		}
 
 		if (GUI.Button (new Rect (50, 500, 300, 100), "登录", myButtonStyle)){
