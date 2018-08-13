d50b7f1be3f5c9cad510839bc43e1334bf6bbc73
sunyi <sunyi@xindong.com>
2018-08-13 17:41:54 +0800

    Update iOSImpl.cs

--- Unity/Assets/Plugins/TapTap/iOS/iOSImpl.cs
+++ Unity/Assets/Plugins/TapTap/iOS/iOSImpl.cs
@@ -12,11 +12,15 @@ namespace com.taptap.sdk {
 	public class iOSImpl : TapTapSDKImpl {
 		
 		public override void OpenTapTapForum(string appid){
+			#if UNITY_IOS && !UNITY_EDITOR
 			openTapTapForum(appid);
+			#endif
 		}
-
+		
+		#if UNITY_IOS && !UNITY_EDITOR
 		[DllImport("__Internal")]
 		private static extern void openTapTapForum (string appid);
+		#endif
 	}
 }
 
