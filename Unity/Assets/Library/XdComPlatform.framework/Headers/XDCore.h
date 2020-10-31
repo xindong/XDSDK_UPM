a57600568da556162e8c4872ef3d179bf11161df
lichao <lichao@taptap.com>
2020-10-31 21:44:26 +0800

    update ios 4.3.3 and change libTapdb jar

--- Unity/Assets/Library/XdComPlatform.framework/Headers/XDCore.h
+++ Unity/Assets/Library/XdComPlatform.framework/Headers/XDCore.h
@@ -13,7 +13,7 @@
 #define XdComPlatform_XDCore_h
 
 /**
- 心动SDK 4.3.2
+ 心动SDK 4.3.3
  */
 @interface XDCore : NSObject
 
--- Unity/Assets/Plugins/script/XDSDK.cs
+++ Unity/Assets/Plugins/script/XDSDK.cs
@@ -1,5 +1,5 @@
 /*
- * XDSDK Version 2.3.1
+ * XDSDK Version 2.3.2
  * 
  * 
  * 
